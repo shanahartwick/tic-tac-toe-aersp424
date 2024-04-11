fe3e49d4feba773c1ffa1cb54c9295051bab735f
Shana <shana.hartwick@gmail.com>
2024-04-11 18:09:41 -0400

    kristi's edits from 3/27

--- main.cpp
+++ main.cpp
@@ -3,18 +3,92 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <vector>
+
+// Function prototypes
+void printBoard(const std::vector<char>& board);
+int getPlayerChoice(const std::vector<char>& board);
+bool makeMove(std::vector<char>& board, int choice, char currentPlayer);
+bool checkWin(const std::vector<char>& board, char currentPlayer);
+bool checkDraw(const std::vector<char>& board);
+
+int main() {
+    std::vector<char> board(9, ' ');
+    char currentPlayer = 'X';
+    bool gameEnded = false;
+
+    while (!gameEnded) {
+        printBoard(board);
+        int choice = getPlayerChoice(board);
+        gameEnded = makeMove(board, choice, currentPlayer);
+
+        if (gameEnded) {
+            std::cout << "Player " << currentPlayer << " wins!" << std::endl;
+            break;
+        }
+        else if (checkDraw(board)) {
+            std::cout << "The game is a draw!" << std::endl;
+            break;
+        }
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+    printBoard(board);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void printBoard(const std::vector<char>& board) {
+    std::cout << "\n";
+    for (int i = 0; i < 9; i += 3) {
+        std::cout << " " << board[i] << " | " << board[i + 1] << " | " << board[i + 2] << "\n";
+        if (i < 6) {
+            std::cout << "-----------\n";
+        }
+    }
+    std::cout << "\n";
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int getPlayerChoice(const std::vector<char>& board) {
+    while (true) {
+        std::cout << "Enter your move (1-9): ";
+        int choice;
+        std::cin >> choice;
+        if (std::cin.fail() || choice < 1 || choice > 9 || board[choice - 1] != ' ') {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid move, try again.\n";
+        }
+        else {
+            return choice;
+        }
+    }
+}
+
+bool makeMove(std::vector<char>& board, int choice, char currentPlayer) {
+    board[choice - 1] = currentPlayer;
+    return checkWin(board, currentPlayer);
+}
+
+bool checkWin(const std::vector<char>& board, char currentPlayer) {
+    const int WINNING_COMBOS[8][3] = {
+        {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+        {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+        {0, 4, 8}, {2, 4, 6}
+    };
+    for (const auto& combo : WINNING_COMBOS) {
+        if (board[combo[0]] == currentPlayer &&
+            board[combo[1]] == currentPlayer &&
+            board[combo[2]] == currentPlayer) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool checkDraw(const std::vector<char>& board) {
+    for (char cell : board) {
+        if (cell == ' ') {
+            return false;
+        }
+    }
+    return true;
+}
